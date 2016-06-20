870651807ab68354e26edf842000eb376c7c6968
etotparen <bandm@tut.by>
2016-06-20 22:24:24 +0300

    Initial commit

---
+++ src/test/java/demo/test/DemoTest.java
@@ -0,0 +1,22 @@
+package demo.test;
+
+
+import webdriver.BaseTest;
+import demo.forms.*;
+
+public class DemoTest extends BaseTest {
+
+	
+	public void runTest() {
+
+		logger.step(1);
+		TutSearchForm tsf = new TutSearchForm();
+		tsf.assertLogo();
+
+		logger.step(2);
+		tsf.searchFor("A1QA");
+
+		logger.step(3);
+		tsf.assertA1QAString();
+	}
+}
